c353c11e701f6b77f9b5135afb833cb280ab1b0d
Vladyslav Shynder <vl.shynder@gmail.com>
2024-03-23 16:33:16 +0000

    Second part

--- app/main.zig
+++ app/main.zig
@@ -8,7 +8,7 @@ pub fn main() !void {
 
     if (args.len < 3) {
         try stdout.print("Usage: your_bittorrent.zig <command> <args>\n", .{});
-        std.os.exit(1);
+        std.process.exit(1);
     }
 
     const command = args[1];
@@ -16,26 +16,56 @@ pub fn main() !void {
     if (std.mem.eql(u8, command, "decode")) {
         // Uncomment this block to pass the first stage
         const encodedStr = args[2];
-        const decodedStr = decodeBencode(encodedStr) catch {
-            try stdout.print("Invalid encoded value\n", .{});
-            std.os.exit(1);
+        const decoder = BencodeDecoder{
+            .encodedValue = encodedStr,
         };
-        var string = std.ArrayList(u8).init(allocator);
-        try std.json.stringify(decodedStr.*, .{}, string.writer());
-        const jsonStr = try string.toOwnedSlice();
-        try stdout.print("{s}\n", .{jsonStr});
+
+        if (decoder.isString()) {
+            const decodedStr = decoder.decodeString() catch {
+                try stdout.print("Invalid encoded value\n", .{});
+                std.process.exit(1);
+            };
+            var string = std.ArrayList(u8).init(allocator);
+            try std.json.stringify(decodedStr.*, .{}, string.writer());
+            const jsonStr = try string.toOwnedSlice();
+            try stdout.print("{s}\n", .{jsonStr});
+        }
+        if (decoder.isNumber()) {
+            const decodedNum = decoder.decodeNumber() catch {
+                try stdout.print("Incalid encoded value\n", .{});
+                std.process.exit(1);
+            };
+            try stdout.print("{}\n", .{decodedNum});
+        }
     }
 }
 
-fn decodeBencode(encodedValue: []const u8) !*const []const u8 {
-    if (encodedValue[0] >= '0' and encodedValue[0] <= '9') {
-        const firstColon = std.mem.indexOf(u8, encodedValue, ":");
+const BencodeDecoder = struct {
+    encodedValue: []const u8,
+
+    const Self = @This();
+
+    fn isString(self: Self) bool {
+        return self.encodedValue[0] >= '0' and self.encodedValue[0] <= '9';
+    }
+
+    fn isNumber(self: Self) bool {
+        return self.encodedValue[0] == 'i';
+    }
+
+    fn decodeString(self: Self) !*const []const u8 {
+        const firstColon = std.mem.indexOf(u8, self.encodedValue, ":");
         if (firstColon == null) {
             return error.InvalidArgument;
         }
-        return &encodedValue[firstColon.? + 1 ..];
-    } else {
-        try stdout.print("Only strings are supported at the moment\n", .{});
-        std.os.exit(1);
+        return &self.encodedValue[firstColon.? + 1 ..];
     }
-}
+
+    fn decodeNumber(self: Self) !i32 {
+        const endOfNum = std.mem.indexOf(u8, self.encodedValue, "e");
+        if (endOfNum == null) {
+            return error.InvalidArgument;
+        }
+        return std.fmt.parseInt(i32, self.encodedValue[1..endOfNum.?], 10);
+    }
+};
